3c67f6b578b9528fe079e53ba2017200626c1a45
Mordokay <papajorgioster@gmail.com>
2018-01-27 17:46:01 +0000

    Movement working

--- Assets/Photon Unity Networking/UtilityScripts/OnJoinedInstantiate.cs
+++ Assets/Photon Unity Networking/UtilityScripts/OnJoinedInstantiate.cs
@@ -20,7 +20,8 @@ public class OnJoinedInstantiate : MonoBehaviour
                     spawnPos = this.SpawnPositions[Random.Range(0, SpawnPositions.Length)].position;
                 }
 
-                PhotonNetwork.Instantiate(o.name, spawnPos, Quaternion.identity, 0);
+                GameObject myPlayer = PhotonNetwork.Instantiate(o.name, spawnPos, Quaternion.identity, 0);
+                Camera.main.GetComponent<CameraFollowPlayer>().player = myPlayer;
             }
         }
     }
