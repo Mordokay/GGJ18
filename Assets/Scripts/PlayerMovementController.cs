cec6bb55ac7f25eefb7ee84871829ab23be8b4ad
Mordokay <papajorgioster@gmail.com>
2018-01-27 20:43:41 +0000

    Player movement updated V10.0

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -11,6 +11,7 @@ public class PlayerMovementController : MonoBehaviour
     private Vector3 TargetPosition;
     //Only used if we want to update rotation in the future
     //private Quaternion TargetRotation;
+    bool recievedTargetPos;
 
     private void Start()
     {
@@ -35,16 +36,32 @@ public class PlayerMovementController : MonoBehaviour
         else
         {
             TargetPosition = (Vector3)stream.ReceiveNext();
+            Debug.Log("Recieved target pos");
+            recievedTargetPos = true;
+            return;
             //TargetRotation = (Quaternion)stream.ReceiveNext();
         }
+
+        recievedTargetPos = false;
     }
 
     private void SmoothMove()
     {
-        if(Vector3.Distance(TargetPosition, transform.position) > 0.05f)
+        if (recievedTargetPos)
         {
             transform.position = Vector3.Lerp(transform.position, TargetPosition, 0.25f);
         }
+        else
+        {
+            transform.position = TargetPosition;
+        }
+        /*
+        if (Vector3.Distance(TargetPosition, transform.position) > 0.2f)
+        {
+            transform.position = Vector3.Lerp(transform.position, TargetPosition, 0.25f);
+            Debug.Log(transform.position + " <> " + TargetPosition + " <distance> " + Vector3.Distance(TargetPosition, transform.position));
+        }
+        */
         //transform.rotation = Quaternion.RotateTowards(transform.rotation, TargetRotation, 500 * Time.deltaTime);
     }
 
@@ -82,6 +99,11 @@ public class PlayerMovementController : MonoBehaviour
         else
         {
             this.GetComponent<Rigidbody2D>().velocity /= 1.05f;
+
+            if (this.GetComponent<Rigidbody2D>().velocity.magnitude < 0.2f)
+            {
+                this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            }
         }
     }
 }
