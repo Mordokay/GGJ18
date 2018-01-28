9c1ad94988d93e7831a2b6b14ea2a363b8c62e1d
Mordokay <papajorgioster@gmail.com>
2018-01-28 02:48:53 +0000

    Sounds

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+	void Update () {
+        if(this.GetComponent<PhotonView>().isMine && !this.GetComponent<AudioSource>().isPlaying)
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+	}
+}
