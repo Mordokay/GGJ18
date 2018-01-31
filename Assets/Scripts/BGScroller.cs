9a7577d557c7d3b91234befc18fd12836554187f
papajorgioster@gmail.com <foquinha44>
2018-01-31 18:31:05 +0000

    Change Lobby UI and scripts. Fixed Lobby Scrollable Backround

--- Assets/Scripts/BGScroller.cs
+++ Assets/Scripts/BGScroller.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class BGScroller : MonoBehaviour {
 
-    public float scrollSpeed;
-    public float tileSizeZ;
+    public float forceSpeed;
 
-    private Vector3 startPosition;
-
-    void Start()
+    private void Start()
     {
-        startPosition = transform.position;
+        Vector3 myPos = new Vector3(0.0f, 0.0f, 0.0f);
+        GameObject myTitlescreen = Instantiate(Resources.Load("Titlescreen"), this.transform) as GameObject;
+        myTitlescreen.transform.position = myPos;
+        myTitlescreen.GetComponent<Rigidbody2D>().AddForce(Vector2.left * forceSpeed);
+
+        myPos = new Vector3(23.0f, 0.0f, 0.0f);
+        myTitlescreen = Instantiate(Resources.Load("Titlescreen"), this.transform) as GameObject;
+        myTitlescreen.transform.position = myPos;
+        myTitlescreen.GetComponent<Rigidbody2D>().AddForce(Vector2.left * forceSpeed);
     }
 
     void Update()
     {
-        float newPosition = Mathf.Repeat(Time.deltaTime * scrollSpeed, tileSizeZ);
-        transform.position = startPosition + Vector3.left * newPosition;
+        
+        if(this.transform.GetChild(this.transform.childCount - 1).position.x < 0.0f)
+        {
+            Vector3 myPos = new Vector3(this.transform.GetChild(this.transform.childCount - 1).position.x + 23.0f, 0.0f, 0.0f);
+            GameObject myTitlescreen = Instantiate(Resources.Load("Titlescreen"), this.transform) as GameObject;
+            myTitlescreen.transform.position = myPos;
+            myTitlescreen.GetComponent<Rigidbody2D>().AddForce(Vector2.left * forceSpeed);
+        }
+
+        if(this.transform.GetChild(0).position.x < -23.0f)
+        {
+            Destroy(this.transform.GetChild(0).gameObject);
+        }
+        
     }
 }
